c4bda95e56ac216912aeab44faefe38755363232
Ahmad Sadati <a.sadati336@gmail.com>
2023-12-24 11:26:55 +0330

    fix of id pages not getting css

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -18,10 +18,10 @@
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic"
         rel="stylesheet" type="text/css" />
     <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="css/styles.css" rel="stylesheet" />
+    <link href="/css/styles.css" rel="stylesheet" />
     <link rel="stylesheet" href="@yield('bootstrap')"
         integrity="sha384-DOXMLfHhQkvFFp+rWTZwVlPVqdIhpDVYT9csOnHSgWQWPX0v5MCGtjCJbY6ERspU" crossorigin="anonymous">
-    <link href="css/custom.css" rel="stylesheet" />
+    <link href="/css/custom.css" rel="stylesheet" />
 </head>
 
 <body id="page-top">
